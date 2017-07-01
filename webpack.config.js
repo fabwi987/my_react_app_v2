af5681fa0a12a7b03c7b34b64f24fa544ab4022e
fabwi987 <fabian.widen@gmail.com>
2017-07-01 18:30:34 +0200

    Workspace set up

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+ entry: './src/js/app.js',
+ output: { 
+     path: __dirname + "/src/", 
+     filename: 'bundle.js' 
+    },
+ watch: true,
+ module: {
+  loaders: [
+   {
+    test: /.jsx?$/,
+    loader: 'babel-loader',
+    exclude: /node-modules/,
+    query: {
+     presets: ['es2015', 'react']
+    }
+   }
+  ]
+ },
+};
